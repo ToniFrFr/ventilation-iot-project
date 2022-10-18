f976ccdc76a0b8ed30091a7257ac1d099d54dc3b
tonifr <toni.franciskovic@metropolia.fi>
2022-10-18 16:03:44 +0300

    MQTT, WebSockets
    Added MQTT operation. Receiving incoming MQTT messages working with
    simulator. Sending MQTT messages has not been tested.
    
    WebSocket operation working.

--- public/scripts/brain.js
+++ public/scripts/brain.js
@@ -1,6 +1,9 @@
+/* Dummy initial values */
 let pressure = 40
 let speed = 15
+
 let intervalID
+let receivedJSON
 
 function getPressure () {
     return pressure
@@ -15,6 +18,51 @@ function setSpeed (targetSpeed) {
     speed = targetSpeed
 }
 
+// WebSocket client
+let client = new WebSocket('ws://localhost:3030')
+
+// Receive message sent from server
+// Receiving MQTT messages containing status updates (MQTT -> SERVER -> CLIENT)
+client.onmessage = (event) => {
+    receivedJSON = JSON.parse(event.data)
+    console.log('ON CLIENT SIDE, received message from server:')
+    console.log(receivedJSON)
+}
+
+/* Send speed button & event listener */
+let btn_sendSpeed = document.getElementById("btn_send_speed")
+btn_sendSpeed.addEventListener('click', (e) => {
+    let targetSpeed = getSpeed()
+
+    // Payload to be sent to 'topic'
+    let payload = {
+        topic:"controller/settings",
+        auto:"false",
+        speed: targetSpeed
+    }
+    client.send(JSON.stringify(payload))
+    console.log('ON CLIENT SIDE: speed send clicked, sent to server:')
+    console.log(JSON.stringify(payload))
+})
+
+/* Send pressure button & event listener */
+let btn_sendPressure = document.getElementById("btn_send_pressure")
+btn_sendPressure.addEventListener('click', (e) => {
+    let targetPressure = getPressure()
+
+    // Payload to be published to 'topic'
+    let payload = {
+        topic:"controller/settings",
+        auto:"true",
+        pressure: targetPressure
+    }
+    client.send(JSON.stringify(payload))
+    console.log('ON CLIENT SIDE: pressure send clicked, sent to server:')
+    console.log(JSON.stringify(payload))
+})
+
+
+/*
 
 // Listen to which tab (mode) is active
 let radioButtons = document.querySelectorAll('input[name="group-one"]')
@@ -24,10 +72,13 @@ radioButtons.forEach((button) => {
     })
 })
 
+*/
+
 // Pressure slider & display
 // AUTOMATIC TAB - UNLOCKED
 let pressureSlider_unlocked = document.getElementById("pressure-slider-automatic")
 let pressurePara_unlocked = document.getElementById("pressure-para-automatic")
+
 pressureSlider_unlocked.oninput = function() {
     setPressure(this.value)
     pressurePara_unlocked.innerHTML = `<b>Pressure (pa):</b> ${this.value}`
@@ -54,7 +105,17 @@ speedSlider_unlocked.oninput = function() {
     speedPara_unlocked.innerHTML = `<b>Fan speed:</b> ${this.value}`
 }
 
-intervalID = setInterval(updateAll, 1000)
+// Temperature displays
+let tempDisplays = document.querySelectorAll('#temp-p')
+
+// Relative humidity displays
+let rhDisplays = document.querySelectorAll('#rh-p')
+
+// Co2 displays
+let co2Displays = document.querySelectorAll('#co2-p')
+
+intervalID = setInterval(updateAll, 2000)
+
 function updateAll() {
     let current_speed = getSpeed()
     let current_pressure = getPressure()
@@ -68,4 +129,9 @@ function updateAll() {
     speedPara_locked.innerHTML = `<b>Fan speed:</b> ${current_speed}`
     speedSlider_unlocked.value = current_speed
     speedPara_unlocked.innerHTML = `<b>Fan speed:</b> ${current_speed}`
+
+    tempDisplays.forEach(display => display.innerHTML = `<b>Temp: </b>${receivedJSON.temp}&deg;C`)
+    rhDisplays.forEach(display => display.innerHTML = `<b>Rh: </b>${receivedJSON.rh}%`)
+    co2Displays.forEach(display => display.innerHTML = `<b>Co2: </b>${receivedJSON.co2}`)
+
 }
