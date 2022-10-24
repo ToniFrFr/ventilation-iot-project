c26a60898e36b1df4ca3cecb6c9e6b00cbd89bd7
samuelti <samuel.tikkanen@metropolia.fi>
2022-10-24 14:19:18 +0300

    Refactor measurements and convert index to ES6 module

--- db/index.mjs
+++ db/index.mjs
@@ -4,51 +4,108 @@
 
 import { Postgres } from './postgres.mjs';
 
-const db = await Postgres.getDb();
+export class Db {
+	constructor(config) {
+		this.config = config;
+	}
 
-export class User {
-	constructor(username) {
+	async connect() {
+		this.db = new Postgres(this.config);
+		await this.db.init();
+
+	}
+
+	getUsers() {
+		try {
+			return new Users(this.db.getMeasurementsTable());
+		} catch(e) {
+			console.error(e);
+			throw new Error("Did you forget to connect to the database?");
+		}
+	}
+
+	getMeasurements() {
+		try {
+			return new Measurements(this.db.getMeasurementsTable());
+		} catch(e) {
+			console.error(e);
+			throw new Error("Did you forget to connect to the database?");
+		}
+	}
+}
+
+class User {
+	constructor(table, username) {
+		this.table = table;
 		this.username = username;
 	}
 
 	async authenticate(password) {
 		try {
-			return await db.authenticate_user(this.username, password);
+			this.table.authenticateUser(this.username, password);
 		} catch(e) {
 			console.error(e);
 			return false;
 		}
 	}
 
-	get capabilities() {
-		return db.get_user_caps(this.username);
+	async getCapabilities() {
+		this.table.getUserCaps(this.username);
 	}
 
-	async grant_capability(capability) {
-		await db.grant_cap(this.username, capability);
+	async grantCapability(capability) {
+		this.table.grantCap(this.username, capability);
 	}
 }
 
 export class Measurement {
 	constructor(msg) {
 		this.nr = msg.nr;
-		this.datetime = msg.datetime;
+		this.datetime = msg.datetime === undefined ? new Date() : msg.datetime;
 		this.pressure = msg.pressure;
 		this.co2 = msg.co2;
-		this.temperature = msg.temperature;
+		this.temp = msg.temp;
 		this.rh = msg.rh;
 		this.speed = msg.speed;
 		this.auto = msg.auto;
 	}
+}
 
-	static async* get_samples(nr_low, nr_high) {
-		for await (let sample of db.get_samples(nr_low, nr_high)) {
+class Users {
+	constructor(table) {
+		this.table = table;
+	}
+
+	async createUser(username, password) {
+		await this.table.createUser(username, password);
+		return new User(this, username);
+	}
+
+	async getUser(username) {
+		return new User(this, username);
+	}
+}
+
+class Measurements {
+	constructor(table) {
+		this.table = table;
+		this.epoch = table.epoch;
+	}
+
+	async* getSamples(epoch, nr_low, nr_high) {
+		for await (let sample of this.table.getSamplesByNr(epoch, nr_low, nr_high)) {
 			yield new Measurement(sample);
 		}
 	}
 
-	async submit() {
-		await db.save_sample(this);
+	async* getSamplesByTime(time_low, time_high) {
+		for await (let sample of this.table.getSamplesByTime(time_low, time_high)) {
+			yield new Measurement(sample);
+		}
+	}
+
+	async submit(sample) {
+		await this.table.saveSample(sample);
 	}
 }
 
--- db/postgres.mjs
+++ db/postgres.mjs
@@ -6,25 +6,6 @@ import * as pg from 'pg';
 const { Pool } = pg.default;
 // pg is not an ES6 module, so needs some massaging to function with import
 
-const usernameLenMax = 32;
-const capLenMax = 16;
-
-const prodConfig = {
-	database: 'iotproject',
-	host: 'db.tikkanen.io',
-	user: 'iotapi',
-	// this object will be passed to the TLSSocket constructor
-	ssl: {
-		rejectUnauthorized: false,
-	},
-}
-
-const devConfig = {
-	database: 'iotproject',
-	host: 'localhost',
-	user: 'iotapi'
-}
-
 class VersionsTable {
 	constructor(db) {
 		this.db = db;
@@ -60,8 +41,8 @@ class VersionsTable {
 }
 
 export class Postgres {
-	constructor() {
-		this.pool = new Pool(devConfig);
+	constructor(config) {
+		this.pool = new Pool(config);
 		// the pool will emit an error on behalf of any idle clients
 		// it contains if a backend error or network partition happens
 		this.pool.on('error', (err, client) => {
@@ -70,23 +51,24 @@ export class Postgres {
 		});
 	}
 
-	static async getDb() {
-		const db = new Postgres();
-		db.versions = await VersionsTable.getTable(db);
-		await db.prepare_table(
+	async init() {
+		const usernameLenMax = 32;
+		const capLenMax = 16;
+		this.versions = await VersionsTable.getTable(this);
+		await this.prepareTable(
 			"users", [`
 			CREATE TABLE IF NOT EXISTS users (
 				username VARCHAR(${usernameLenMax}) NOT NULL UNIQUE PRIMARY KEY,
 				password TEXT NOT NULL
 			);`]);
-		await db.prepare_table(
+		await this.prepareTable(
 			"capabilities", [`
 			CREATE TABLE IF NOT EXISTS capabilities (
 				username VARCHAR(${usernameLenMax}) references users(username),
 				capability VARCHAR(${capLenMax}) NOT NULL,
 				PRIMARY KEY(username, capability)
 			);`]);
-		await db.prepare_table(
+		await this.prepareTable(
 			"measurements", [`
 			CREATE TABLE IF NOT EXISTS measurements (
 				nr INT PRIMARY KEY,
@@ -97,15 +79,26 @@ export class Postgres {
 				rh SMALLINT,
 				speed SMALLINT,
 				auto BOOLEAN
-			);`]);
-		await db.prepare_table(
+			);`,
+			`
+			ALTER TABLE measurements DROP CONSTRAINT measurements_pkey;
+			ALTER TABLE measurements ADD COLUMN epoch SMALLINT;
+			UPDATE measurements SET epoch = 0;
+			ALTER TABLE measurements ADD PRIMARY KEY(epoch, nr);`,
+			`
+			ALTER TABLE measurements RENAME COLUMN temperature TO temp;`,
+			`
+			ALTER TABLE measurements ALTER COLUMN pressure TYPE DECIMAL(6, 1);
+			ALTER TABLE measurements ALTER COLUMN temp TYPE DECIMAL(6, 1);
+			ALTER TABLE measurements ALTER COLUMN rh TYPE DECIMAL(6, 1);
+			ALTER TABLE measurements ALTER COLUMN speed TYPE DECIMAL(6, 1);`]);
+		await this.prepareTable(
 			"authentication_log", [`
 			CREATE TABLE IF NOT EXISTS authentication_log (
 				eventId SERIAL PRIMARY KEY,
 				datetime TIMESTAMP WITH TIME ZONE,
 				username VARCHAR(${usernameLenMax}) references users(username)
 			);`]);
-		return db;
 	}
 
 	async run(queryText, args = []) {
@@ -122,7 +115,7 @@ export class Postgres {
 		}
 	}
 
-	async prepare_table(name, migrations) {
+	async prepareTable(name, migrations) {
 		try {
 			const ver = await this.versions.version(name);
 			const mlen = migrations.length;
@@ -135,6 +128,9 @@ export class Postgres {
 						ON CONFLICT (name) DO UPDATE SET version = $2;
 						`, [name, i + 1]);
 				}
+			} else if(ver > mlen) {
+				console.error(`Table ${name} is newer than we are. Try updating to the latest release.`);
+				throw new Error("Table version is newer than any known to the code.");
 			} else {
 				console.log(`Table ${name} is up to date`);
 			}
@@ -144,8 +140,24 @@ export class Postgres {
 		}
 	}
 
-	async create_user(username, password) {
-		const client = await this.pool.connect();
+	getUsersTable() {
+		return new UsersTable(this);
+	}
+	
+	getMeasurementsTable() {
+		console.log(this);
+		console.log(this.db);
+		return new MeasurementsTable(this);
+	}
+}
+
+class UsersTable {
+	constructor(db) {
+		this.db = db;
+	}
+
+	async createUser(username, password) {
+		const client = await this.db.pool.connect();
 		try {
 			await client.query('BEGIN');
 			const queryText = `
@@ -161,13 +173,13 @@ export class Postgres {
 		}
 	}
 
-	async authenticate_user(username, password) {
+	async authenticateUser(username, password) {
 		const queryText = `
 		SELECT (password = crypt($2, password))
 		AS pswmatch
 		FROM users
 		WHERE username = $1;`;
-		const client = await this.pool.connect();
+		const client = await this.db.pool.connect();
 		try {
 			await client.query('BEGIN');
 			const res = await client.query(queryText, [username, password]);
@@ -184,8 +196,8 @@ export class Postgres {
 		}
 	}
 
-	async* get_user_caps(username) {
-		const client = await this.pool.connect();
+	async* getUserCaps(username) {
+		const client = await this.db.pool.connect();
 		try {
 			await client.query('BEGIN');
 			const queryText = `
@@ -204,29 +216,51 @@ export class Postgres {
 		}
 	}
 
-	async grant_cap(username, capability) {
-		await this.run(`
+	async grantCap(username, capability) {
+		await this.db.run(`
 			INSERT INTO capabilities (username, capability)
 			VALUES ($1, $2);`, [username, capability]);
 	}
+}
 
-	async save_sample(sample) {
-		await this.run(`
-			INSERT INTO measurements
-			(nr, datetime, pressure, co2, temperature, rh, speed, auto)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
-			`, [sample.nr, sample.datetime, sample.pressure, sample.co2, sample.temperature,
-				sample.rh, sample.speed, sample.auto]);
+class MeasurementsTable {
+	constructor(db) {
+		console.log(db);
+		this.db = db;
+		this.epoch = 0;
 	}
 
-	async* get_samples(nr_low, nr_high) {
-		const client = await this.pool.connect();
+	async getEpoch() {
+		return await this.db.run(`SELECT MAX(epoch) FROM measurements;`);
+	}
+	
+	async getPreviousNr(epoch) {
+		return await this.db.run(
+			`SELECT MAX(nr) FROM measurements WHERE epoch = $1;`,
+			[epoch]);
+	}
+
+	async saveSample(sample) {
+		if(sample.nr < await this.getPreviousNr(await this.getEpoch())) {
+			this.epoch++;
+		}
+		await this.db.run(`
+			INSERT INTO measurements
+			(epoch, nr, datetime, pressure, co2, temp, rh, speed, auto)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+			`, [this.epoch, sample.nr, sample.datetime, sample.pressure, sample.co2,
+				sample.temp, sample.rh, sample.speed, sample.auto]);
+	}
+
+	async* getSamplesByNr(epoch, nr_low, nr_high) {
+		console.log(this.db);
+		const client = await this.db.pool.connect();
 		try {
 			await client.query('BEGIN');
 			const queryText = `
 			SELECT * FROM measurements
-			WHERE nr >= $1 AND nr <= $2;`
-			const res = await client.query(queryText, [nr_low, nr_high]);
+			WHERE epoch = $1 AND nr >= $2 AND nr <= $3;`
+			const res = await client.query(queryText, [epoch, nr_low, nr_high]);
 			await client.query('COMMIT');
 			for await(let row of res.rows) {
 				yield row;
@@ -239,6 +273,24 @@ export class Postgres {
 		}
 	}
 
+	async* getSamplesByTime(time_low, time_high) {
+		const client = await this.db.pool.connect();
+		try {
+			await client.query('BEGIN');
+			const queryText = `
+			SELECT * FROM measurements
+			WHERE datetime >= $2 AND datetime <= $3;`
+			const res = await client.query(queryText, [time_low, time_high]);
+			await client.query('COMMIT');
+			for await(let row of res.rows) {
+				yield row;
+			}
+		} catch(e) {
+			await client.query('ROLLBACK');
+			throw e;
+		} finally {
+			client.release();
+		}
+	}
 }
-
 
