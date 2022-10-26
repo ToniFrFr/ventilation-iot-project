e87b160731229ef22994a2781ce10fc75b8e4db4
samuelti <samuel.tikkanen@metropolia.fi>
2022-10-26 16:34:10 +0300

    Add sessions, authentication, and combine ws and http to same port

---
+++ auth.mjs
@@ -0,0 +1,27 @@
+import expressSession from 'express-session';
+import pgSession from 'connect-pg-simple';
+
+export function getSession(db) {
+	if(!('COOKIE_SECRET' in process.env)) {
+		console.error("Variable COOKIE_SECRET required in the environment");
+		process.exit(-1);
+	}
+
+	const session = pgSession(expressSession);
+	return expressSession({
+		store: new session({
+			pool: db.db.pool,
+			createTableIfMissing: true
+		}),
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+			secure: true,
+			httpOnly: true
+		}
+	});
+}
+
+
